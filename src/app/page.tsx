39df9e28d330e05de465f1b2debcc8d66a6d2889
ArthurViniciusL <arthurviniciussl.contato@gmail.com>
2024-09-12 15:13:14 -0300

    Add font face

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main>
+    <main className="app-main">
       <h1>Olá</h1>
+      <h2>Olá de novo</h2>
+      <button className="app-button">Clique</button>
     </main>
   );
 }
