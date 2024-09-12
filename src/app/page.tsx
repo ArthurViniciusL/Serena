8bbc58b4bf47fd8532ecc39dd522662a690640ec
ArthurViniciusL <arthurviniciussl.contato@gmail.com>
2024-09-12 19:14:28 -0300

    Start of creating the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,228 @@
+import "@/app/styles/home_styles.css";
+
 import { Button } from "./components/Button";
 import { Header } from "./components/Header";
+import { SerenaLogo } from "./components/SerenaLogo";
+import { CirclePlusIcon } from "./modules/app.modules";
+import { Tooltip } from "./components/Tooltip";
 
 export default function Home() {
     return (
         <div>
             <Header colorFill="var(--bgColor-02)">
-                <h1>Olá</h1>
+                <div className="home-header__content">
+                    <SerenaLogo />
+                    <ul className="buttons-list">
+                        <li>
+                            <Button
+                                bgColorHover="var(--bgColor-01)"
+                                fontColorHover="var(--fontColor-01)"
+                            >
+                                Sobre
+                            </Button>
+                        </li>
+                        <li>
+                            <Button
+                                bgColorHover="var(--bgColor-01)"
+                                fontColorHover="var(--fontColor-01)"
+                            >
+                                Recursos
+                            </Button>
+                        </li>
+                        <li>
+                            <Button
+                                bgColorHover="var(--bgColor-01)"
+                                fontColorHover="var(--fontColor-01)"
+                            >
+                                Preços
+                            </Button>
+                        </li>
+
+                        <li>
+
+
+                            <Tooltip msg="AAAAAAAAAAAAAAA">
+                                <Button bgColorHover="var(--bgColor-01)">
+                                    <CirclePlusIcon color="var(--fontColor-01)" />
+                                </Button>
+                            </Tooltip>
+                        </li>
+                    </ul>
+                </div>
+
                 <Button
-                    bgColorHover="var(--bgColor-01)"
+                    bgColorHover="var(--red-01)"
                     fontColorHover="var(--fontColor-01)"
                 >
                     clique aqui!
                 </Button>
             </Header>
             <main className="app-main">
-                <Button
-                    bgColorHover="var(--bgColor-01)"
-                    fontColorHover="var(--fontColor-01)"
-                >
-                    clique aqui!
-                </Button>
-
+                <Button>clique aqui!</Button>
 
                 <div>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
 
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam at, cum odit, ad soluta sunt assumenda beatae qui iusto expedita quod nostrum consectetur error voluptatibus repellendus illum dolorem, non amet!</p>
-
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Veniam at, cum odit, ad soluta sunt assumenda
+                        beatae qui iusto expedita quod nostrum consectetur error
+                        voluptatibus repellendus illum dolorem, non amet!
+                    </p>
                 </div>
+
+                {/* 
+                <section></section>
+                */}
             </main>
         </div>
     );
