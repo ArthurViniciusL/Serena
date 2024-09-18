2ea6eaa633b32577beeb4c91171056d37917d40d
ArthurViniciusL <arthurviniciussl.contato@gmail.com>
2024-09-18 16:26:32 -0300

    created the feed page component

---
+++ src/app/pages/feed/page.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@/app/components/Header";
+import { LogoTipo } from "@/app/components/SerenaLogos/LogoTipo";
+
+export default function Feed() {
+    return (
+        <main className="app-main">
+            
+        </main>
+    )
+}
