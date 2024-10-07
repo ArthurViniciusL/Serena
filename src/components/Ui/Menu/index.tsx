f75a7d93adf96d12852fc2c5d156001e0728a78d
ArthurViniciusL <arthurviniciussl.contato@gmail.com>
2024-10-07 20:09:26 -0300

    Implemented the functional part of the Menu component

--- src/components/Ui/Menu/index.tsx
+++ src/components/Ui/Menu/index.tsx
@@ -7,10 +7,11 @@ import { Button } from "@/components/Button";
 interface MenuProps {
     children: React.ReactNode;
 }
-/* esse é menu que tá lá no feed */
+
 export function Menu({ children }: MenuProps) {
 
-    const [screen, setScreen] = useState(800);
+    const [screen, setScreen] = useState<number>(800);
+    const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false)
 
     useEffect(() => {
         function updateScreenSize() {
@@ -18,7 +19,12 @@ export function Menu({ children }: MenuProps) {
             // console.log(`Nova largura da tela: ${screen}px`);
         }
         window.addEventListener('resize', updateScreenSize);
-    })
+    });
+
+    function handleMenuIsClick() {
+        setMenuIsOpen(!menuIsOpen)
+    }
+
     /* screen >= 770 */
     if (screen >= 770) {
         return (
@@ -35,20 +41,26 @@ export function Menu({ children }: MenuProps) {
             <>
                 {/* é provisório ok, isso vai virar um outro componente chamado "MobileMenu" que carrega um children */}
 
-                <Button>
-                    <AlignJustify size={20} />
-                </Button>
-                <div className={menu.mobile}>
-                    <span className={menu.closeContent}>
-                        <Button>
-                            <X />
-                        </Button>
-                    </span>
-                    <nav className={''}>
-                        {children}
-                    </nav>
 
-                </div>
+                {
+                    menuIsOpen ?
+                        <main className={menu.mobile}>
+                                <span className={menu.closeContent}>
+                                    <Button onClick={handleMenuIsClick}>
+                                        <X />
+                                    </Button>
+                                </span>
+                                <nav className={''}>
+                                    {children}
+                                </nav>
+                        </main>
+
+                        :
+
+                        <Button onClick={handleMenuIsClick}>
+                            <AlignJustify size={20} />
+                        </Button>
+                }
             </>
         );
     }
